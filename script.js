23761a4eba8e087046a5b182b501c6c982e42123
Johão Pedro <johaopedro99@gmail.com>
2021-11-27 00:51:52 -0300

    estilização feita e mais trocadilhos adicionados

--- script.js
+++ script.js
@@ -1,10 +1,25 @@
+const card = document.getElementById("card")
+const cardButton = document.getElementById("chooseButton")
+const novaPergunta = document.createElement("h3")
+const novaResposta = document.createElement("p")
+
 function gerarNumero(min, max) {
    min = Math.ceil(1);
    max = Math.floor(trocadilhos.length);
    return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-function card(){
-    let posicao = gerarNumero()
-    return trocadilhos[posicao]
+function chooseCard() {
+    let posicao = gerarNumero();
+    return trocadilhos[posicao];
 }
+
+cardButton.addEventListener("click", function(){
+    let trocadilhoAtual = chooseCard()
+    let question = trocadilhoAtual.pergunta
+    let answer = trocadilhoAtual.resposta
+    novaPergunta.innerHTML = question
+    novaResposta.innerHTML = answer
+    card.appendChild(novaPergunta)
+    card.appendChild(novaResposta)
+})
